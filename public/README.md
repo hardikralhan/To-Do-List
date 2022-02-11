86d7a639157e502662be2dab8105f28cf1bda4b6
Hardik <hardikralhan66@gmail.com>
2022-02-11 23:59:21 +0530

    Initial commit

---
+++ public/README.md
@@ -0,0 +1,1 @@
+To Do List Wev app using ReactJS
---
+++ src/components/App.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+function App() {
+  const [itemName, setItemName] = useState("");
+  const [item, setItem] = useState([]);
+
+  function handleChange(event) {
+    const value = event.target.value;
+    setItemName(value);
+  }
+
+  function handleClick() {
+    setItem((prevItems) => {
+      // prevItems array hai
+      return [...prevItems, itemName]; // purani list rahe whenever you click button and new item should add
+    });
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input type="text" onChange={handleChange} value={itemName} />
+        <button onClick={handleClick}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {item.map((itemList) => {
+            return <li>{itemList}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
